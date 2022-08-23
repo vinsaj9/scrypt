12c5fd78219db042cb07987cce7cafeaf1f30804
vinsaj9 <65157049+vinsaj9@users.noreply.github.com>
2022-08-23 13:22:30 +0530

    Update Pbkdf2.php

--- src/Pbkdf2.php
+++ src/Pbkdf2.php
@@ -8,7 +8,7 @@
  * @license http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Cast\Crypto\Scrypt;
+namespace Vinsaj9\Crypto\Scrypt;
 
 /**
  * PKCS #5 v2.0 standard RFC 2898
